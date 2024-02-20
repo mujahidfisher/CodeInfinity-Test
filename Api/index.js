6917990223c375298426356728196a6611789efd
unknown <fishermujahid@gmail.com>
2024-02-20 12:00:36 +0200

    backend completed

--- Api/index.js
+++ Api/index.js
@@ -34,3 +34,47 @@ app.get("/users", (req, res) => {
       res.status(500).json({ error: "Could'nt fetch data!" });
     });
 });
+
+app.get("/user/:id", (req, res) => {
+    const { id } = req.params;
+    if (ObjectId.isValid(req.params.id)) {
+      db.collection("users")
+        .findOne({ _id: new ObjectId(id) })
+        .then((doc) => {
+          res.status(200).json(doc);
+        })
+        .catch((err) => {
+          res.status(500).json({ error: "Could'nt fetch data." });
+        });
+    } else {
+      res.status(500).json({ error: "Not a valid data id" });
+    }
+  });
+
+app.post("/register", (req, res) => {
+  const USER = req.body;
+  db.collection("users")
+    .insertOne(USER)
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((err) => {
+      res.status(500).status({ error: "Error could not post" });
+    });
+});
+
+app.delete("/delete/:id", (req, res) => {
+  const { id } = req.params;
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("users")
+      .deleteOne({ _id: new ObjectId(id) })
+      .then((result) => {
+        res.status(200).json(result);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "Could'nt fetch data." });
+      });
+  } else {
+    res.status(500).json({ error: "Not a valid data id" });
+  }
+});
